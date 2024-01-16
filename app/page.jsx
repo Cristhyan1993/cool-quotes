67a840cf74542356334b081f77ca15f958b92d1b
Cristhyan <cristhyancalderon@hotmail.co.uk>
2024-01-16 21:25:43 +0000

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Feed from '../components/Feed'
+
+const Home = () => {
+  return (<>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center" >
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="blue_gradient text-center">Quotes</span>
+      </h1>
+      <p className="desc text-center">
+        Cool Quotes is online environment where users can
+        discover, create and share creative quotes
+      </p>
+      <Feed></Feed>
+    </section>
+    
+   </>
+  );
+};
+
+export default Home;
