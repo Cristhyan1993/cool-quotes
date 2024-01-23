31b1e9d231425d59cb3771a11f8e19d552142c56
Cristhyan <cristhyancalderon@hotmail.co.uk>
2024-01-23 15:01:56 +0000

    cahnged text in home amd my profile

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Feed from '../components/Feed'
 
 const Home = () => {
-  return (<>
+  return (
+  <>
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center" >
         Discover & Share
@@ -11,11 +12,10 @@ const Home = () => {
       </h1>
       <p className="desc text-center">
         Cool Quotes is online environment where users can
-        discover, create and share creative quotes
+        discover, create and share their favourite quotes
       </p>
-      <Feed></Feed>
+      <Feed/>
     </section>
-    
    </>
   );
 };
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -69,7 +69,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={handleEdit} >Edit</p>
           <p
-            className="font-inter text-sm orange_gradient cursor-pointer"
+            className="font-inter text-sm red_gradient cursor-pointer"
             onClick={handleDelete}
           >
             Delete
